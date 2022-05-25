135040ac3d29df327b48692b4fb5e703d096c7cd
Rajul Sahu <74370799+RajulSahu@users.noreply.github.com>
2022-05-25 16:11:41 +0530

    Add files via upload

---
+++ InsertSort.C
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<conio.h>
+
+// Insertion sort
+
+void sort(int nums[10], int n){
+    int i, key;
+    for(i = 1; i < n; i++){
+        int j = i - 1;
+        key = nums[i];
+        while(j >= 0 && key < nums[j]){
+            nums[j+1] = nums[j];
+            j--;
+        }
+        nums[j+1] = key;
+    }
+}
+
+void inputArray(int nums[10], int n){
+    int i;
+    printf("\nEnter the Elements of the array: \n");
+    for(i = 0; i < n; i++){
+        scanf("%d ", &nums[i]);
+    }
+}
+
+void displayArray(int nums[10], int n){
+    int i;
+    printf("\nThe Elements of the array are: \n");
+    for(i = 0; i < n; i++){
+        printf("%d ", nums[i]);
+    }
+}
+
+void main(){
+    int nums[10];
+    clrscr();
+    inputArray(nums,10);
+    printf("\nBefore Sorting: ");
+    displayArray(nums, 10);
+    
+    printf("\nAfter Sorting: ");
+    sort(nums, 10);
+    displayArray(nums, 10);
+    getch();
+}
