2c65bf4c4a4bc68f9f8de9f4543672c3a784743f
Rajul Sahu <74370799+RajulSahu@users.noreply.github.com>
2022-06-09 22:33:55 +0530

    Update BUBBLEST.C

--- BUBBLEST.C
+++ BUBBLEST.C
@@ -1,50 +1,50 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<conio.h>
-void inputArray(int nums[10])
-{
-	int i;
-	printf("Enter 10 elements in the array \n");
-	for(i = 0; i < 10; i++){
-		scanf("%d", &nums[i]);
-	}
+#define SIZE 10
+
+void inputArray(int *nums, int n){
+    int i;
+    printf("\nPlease enter %d elements in the array: \n", n);
+    for(i = 0; i < n; i++){
+        scanf("%d", &nums[i]);
+    }
 }
-void displayArray(int nums[10])
-{
-	int i;
-	printf("The elements of the array are: \n");
-	for(i = 0; i < 10; i++){
-		printf("%d ", nums[i]);
-	}
+
+void displayArray(int *nums, int n){
+    int i;
+    printf("\nThe Array elements are: \n");
+    for(i = 0; i < n; i++){
+        printf("%d ", nums[i]);
+    }
 }
-void swap(int nums[10], int i, int j)
-{
-	int temp;
-	temp = nums[i];
-	nums[i] = nums[j];
-	nums[j] = temp;
+
+void swap(int *nums , int i, int j){
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
 }
-void sort(int nums[10]){
-	int i, j, swapped;
-	for(i = 0; i < 10; i++){
-		swapped = 0;
-		for(j = 0; j < 9-i; j++){
-			if(nums[j] > nums[j+1]){
-				swapped = 1;
-				swap(nums, j, j+1);
-			}
-		}
-		if(!swapped) break;
-	}
+
+void bubbleSort(int *nums, int n){
+    int i, j;
+    int swapped;
+    for(i = 0; i < n; i++){
+        swapped = 0;
+        for(j = 0; j< n-1-i; j++){
+            if(nums[j] > nums[j+1]) {
+                swapped = 1;
+                swap(nums, j, j+1);
+            }
+        }
+        if(!swapped) break;
+    }
 }
-void main()
-{
-	int nums[10];
-	clrscr();
-	inputArray(nums);
-	printf("\nBefore Sorting: \n");
-	displayArray(nums);
-	sort(nums);
-	printf("\nAfter sorting: \n");
-	displayArray(nums);
-	getch();
+
+void main(){
+    int nums[SIZE];
+    inputArray(nums, SIZE);
+    printf("\nBefore Sorting: ");
+    displayArray(nums, SIZE);
+    printf("\nAfter Sorting: ");
+    bubbleSort(nums, SIZE);
+    displayArray(nums, SIZE);
 }
