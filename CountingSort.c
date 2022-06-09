b1d4da99b9277e9d7ac84489e2628fdc960455f0
Rajul Sahu <74370799+RajulSahu@users.noreply.github.com>
2022-06-10 02:50:59 +0530

    Update CountingSort.c

--- CountingSort.c
+++ CountingSort.c
@@ -1,39 +1,50 @@
-#include <stdio.h>
+#include<stdio.h>
 #define SIZE 20
 
-void displayArray(int[], int);
-void countingSort(int[], int);
+
+void displayArray(int *nums, int n){
+    int i;
+    printf("\nThe Array elements are: \n");
+    for(i = 0; i < n; i++){
+        printf("%d ", nums[i]);
+    }
+}
+
+void countingSort(int *nums, int n, int range){
+    range++;
+    int freq[range];
+    int output[SIZE];
+    int i;
+    
+    // Fill zeros in frequency array
+    for (i = 0; i < range; i++) {
+        freq[i] = 0;
+    }
+    
+    // Fill frequencies of each elements
+    for(i = 0; i < n; i++){
+        freq[nums[i]]++;
+    }
+    
+    // Find Cumulative frequencies
+    for(i = 1; i < range; i++){
+        freq[i] = freq[i]+freq[i-1];
+    }
+    
+    // Filling th output Array
+    for(i = n-1; i >= 0; i--){
+        output[freq[nums[i]]-1] = nums[i];
+        freq[nums[i]]--;
+    }
+    displayArray(output, SIZE);
+}
+
 void main()
 {
     int nums[SIZE] = {5,4,3,2,3,4,5,1,3,1,4,5,6,8,6,7,7,1,5,3};
     int range = 8;
-    countingSort(nums, range);
-}
-void countingSort(int nums[], int range){
-    int freq[range+1];
-    int out[SIZE+1];
-    int i;
-    for(i = 0; i < range+1; i++){
-        freq[i] = 0;
-    }
-    for(i = 0; i < SIZE; i++){
-        freq[nums[i]]++;
-    }
-    // displayArray(nums, SIZE);
-    for(i = 0; i < range+1; i++){
-        freq[i] = freq[i] + freq[i-1];
-    }
-    // displayArray(freq, SIZE);
-    for(i = 0; i < SIZE; i++){
-        out[freq[nums[i]]] = nums[i];
-        freq[nums[i]]--;
-    }
-    displayArray(out, SIZE+1);
-}
-void displayArray(int nums[], int size){
-    int i;
-    printf("\nThe array is: \n");
-    for(i = 1; i < size; i++){
-        printf("%d ", nums[i]);
-    }
+    printf("\nBefore Sorting: ");
+    displayArray(nums, SIZE);
+    printf("\nAfter Sorting: ");
+    countingSort(nums, SIZE, range);
 }
